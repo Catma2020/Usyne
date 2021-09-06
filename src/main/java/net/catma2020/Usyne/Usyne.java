65c26b80967c01cd2f83c8a0f88448fc29deac4c
Anthony Craft <crafttanthony@outlook.com>
2021-09-05 20:16:21 -0400

    Reconstructed and repackaged giving a blank template for 1.17.1

---
+++ src/main/java/net/catma2020/Usyne/Usyne.java
@@ -0,0 +1,24 @@
+package net.catma2020.Usyne;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Usyne implements ModInitializer
+{
+
+
+
+	@Override
+	public void onInitialize()
+	{
+
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		System.out.println("Hello Fabric world! This is the mod Usyne by Catma2020!");
+
+		
+
+	}
+
+}
